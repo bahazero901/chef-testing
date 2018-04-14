23b658a1d857804ef3c3eb9f977514466c6ed20d
Matt Feng <matt.feng@yahoo.com>
2018-04-14 12:32:15 -0700

    adding mywebapp recipe

---
+++ cookbooks/mywebapp/recipes/default.rb
@@ -0,0 +1,65 @@
+#
+# Cookbook:: mywebapp
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+node.default['webapp']['user'] = 'earlwaud'
+node.default['webapp']['group'] = 'wheel'
+
+
+#mkdir -p /var/www/users
+directory '/var/www/users' do
+  recursive true
+  mode 0755
+  user node['webapp']['user']
+  group node['webapp']['group']
+  action :create
+end
+
+file '/var/www/users/index.html' do
+  content '<html><title>Users</title><body><h1>Hello to our Users</
+   h1></body></html>'
+  mode 0755
+  user node['webapp']['user']
+  group node['webapp']['group]
+  notifies : restart, 'service[httpd]'
+end
+
+directory '/var/www/suppliers' do
+  recursive true
+  mode 0755
+  user node['webapp']['user']
+  group node['webapp']['group]
+  action :create
+end
+
+file '/var/www/suppliers/index.html' do
+  content '<html><title>Suppliers</title><body><h1>Hello to our
+  Suppliers</h1></body></html>'
+  mode 0755
+  user 'earlwaud'
+  group 'wheel'
+  notifies :restart, 'service[httpd]'
+end
+   
+directory '/var/www/customers' do
+  recursive true
+  mode 0755
+  user 'earlwaud'
+  group 'wheel'
+  action :create
+end  
+
+file '/var/www/customers/index.html' do
+  content '<html><title>Customers</title><body><h1>Hello to our
+Customers</h1></body></html>'
+  mode 0755
+  user 'earlwaud'
+  group 'wheel'
+  notifies :restart, 'service[httpd]'
+end
+
+service 'httpd' do
+  action :nothing
+end
